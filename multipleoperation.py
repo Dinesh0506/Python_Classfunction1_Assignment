0ba78736b52cd8cc08b85c2311add1e8c97847ee
Dineshkumar S <61445369+Dinesh0506@users.noreply.github.com>
2023-02-18 21:29:01 +0530

    Add files via upload

---
+++ multipleoperation.py
@@ -0,0 +1,62 @@
+class multipleoperation():
+    def subfields():
+        print('sub-fields in AI are:')
+        print('Machine Learning')
+        print('Neural networks')
+        print('Vision')
+        print('Robotics')
+        print('Speech Processing')
+        print('Natural Language Processing')
+    def oddeven():
+        num=int(input('enter a number'))
+        if (num % 2)==0:
+            print(num,'is even number')
+        else:
+            print(num,'is odd number')
+    def Eleigle():
+        gender=input('enter a gender')
+        age=int(input('enter a age'))
+        if(gender=='male'):
+            print('your gender:',gender)
+        else:
+            print('your gender:',gender)
+        if(age>18):
+            print("your age:",age)
+            print('eleigle')
+        else:
+            print(age,"not eleigle")
+    def percentage():
+        subject1=int(input('enter subject1'))
+        subject2=int(input('enter subject2'))
+        subject3=int(input('enter subject3'))
+        subject4=int(input('enter subject4'))
+        subject5=int(input('enter subject5'))
+        print('subject1=',subject1)
+        print('subject2=',subject2)
+        print('subject3=',subject3)
+        print('subject4=',subject4)
+        print('subject5=',subject5)
+        total=subject1+subject2+subject3+subject4+subject5
+        percent=total/5
+        print('total:',total)
+        print('percent:',percent)
+    def triangle():
+        Height = float(input('enter a height:'))
+        print(Height)
+        Breadth = float(input('enter a breadth:'))
+        print(Breadth)
+        Height1 = float(input('enter a height1:'))
+        print(Height1)
+        Height2 = float(input('enter a height2:'))
+        print(Height2)
+        Breadth1 = float(input('enter a breadth1:'))
+        print(Breadth1)
+        print('height:',Height)
+        print("breadth:",Breadth)
+        area=Height*Breadth/2
+        print('Area of triangle:',area)
+        print('height1:',Height1)
+        print('height2:',Height2) 
+        print("breadth1:",Breadth1)
+        parameter = Height1+Height2+Breadth1
+        print('parameter of triangle:',parameter)
